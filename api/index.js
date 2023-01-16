07c9fe6a8a848efbef714ca92aa6bff72c7f1248
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-01-16 15:50:36 +0100

    Uten cache-header

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ let cachedData = {
     retrieved: 0
 }
 export default async function handler(req, res) {
-    res.setHeader('Cache-Control', 's-maxage=3600')
+    // res.setHeader('Cache-Control', 's-maxage=3600')
     // Hvis mer enn en time gammel
     if ((Date.now() - cachedData.retrieved) > 3600000 || typeof cachedData.error !== 'undefined') {
         fetch("https://driftsdata.statnett.no/restapi/Physicalflow/GetData?From=2023-01-01")
