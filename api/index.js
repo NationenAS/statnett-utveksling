8bd4a25acf605922f14d526ceeb743b87e64a3a3
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-01-16 10:40:31 +0100

    Lagt inn "retrieved/data" i objekt

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 export default async function handler(req, res) {
+    res.setHeader('Cache-Control', 's-maxage=86400')
     fetch("https://driftsdata.statnett.no/restapi/Physicalflow/GetData?From=2023-01-01")
         .then(r => r.json())
         .then(d => {
+            let data = {
+                retrieved: Date.now(),
+                data: d
+            }
             res.send(d)
         })
         .catch(e => { console.log(e) })
