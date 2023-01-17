45a1818edd3b034c76e0c44b84c1b91416a52f11
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-01-17 08:47:46 +0100

    Enabled Cors

--- api/index.js
+++ api/index.js
@@ -2,8 +2,14 @@ let cachedData = {
     retrieved: 0
 }
 export default async function handler(req, res) {
-    // res.setHeader('Cache-Control', 's-maxage=3600')
-    // Hvis mer enn en time gammel
+    // Enable CORS
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+    // Enable cache
+    res.setHeader('Cache-Control', 's-maxage=3600')
+    // If more than 1 hour old
     if ((Date.now() - cachedData.retrieved) > 3600000 || typeof cachedData.error !== 'undefined') {
         fetch("https://driftsdata.statnett.no/restapi/Physicalflow/GetData?From=2023-01-01")
             .then(r => r.json())
