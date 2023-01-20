f179f6f5bb8d2f406237bc42c48b4fd2f4e3d0f4
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-01-20 11:44:48 +0100

    /countries

---
+++ api/countries.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+    
+    // Enable CORS
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+    
+    // Enable cache
+    res.setHeader('Cache-Control', 's-maxage=14400') // 4 hours
+
+    let countries = [ "se", "dk", "en", "de" ]
+    let from = req.body.from || "2023-01-01"
+    let urlBase = "https://driftsdata.statnett.no/restapi/Physicalflow/GetData?From="
+    let urls = countries.map(c => urlBase + from + "&Country=" + c)
+    let responses = []
+
+    Promise.all(urls.map(url => fetch(url)
+        .then(resp => resp.json()).then(data => responses.push(data))
+    ))
+    .then(
+        res.send(responses)
+    )
+}
