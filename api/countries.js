52e46ee91eedbf595d4d7eee419edd4144acd771
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-01-20 12:52:28 +0100

    Get and post from

--- api/countries.js
+++ api/countries.js
@@ -10,8 +10,9 @@ export default async function handler(req, res) {
     res.setHeader('Cache-Control', 's-maxage=14400') // 4 hours
 
     const countries = [ "se", "dk", "en", "de" ]
-    const { body } = req
-    const from = (typeof body.from !== 'undefined') ? body.from : "2023-01-01"
+    let from = "2023-01-01"
+    req.body && req.body.from && (from = req.body.from)
+    req.query && req.query.from && (from = req.query.from)
     const urlBase = "https://driftsdata.statnett.no/restapi/Physicalflow/GetData?From="
     const urls = countries.map(c => urlBase + from + "&Country=" + c)
     let requests = urls.map(url => fetch(url))
